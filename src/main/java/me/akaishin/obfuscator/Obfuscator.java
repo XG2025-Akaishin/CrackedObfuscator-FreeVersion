b5ccaf88154525117b619746ee2e692d349a9114
akais <akaishin.luis25@gmail.com>
2025-04-17 20:03:30 -0500

    crack obf 9838rjf

--- src/main/java/me/akaishin/obfuscator/Obfuscator.java
+++ src/main/java/me/akaishin/obfuscator/Obfuscator.java
@@ -2,6 +2,7 @@ package me.akaishin.obfuscator;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.util.*;
@@ -15,14 +16,20 @@ import org.objectweb.asm.ClassWriter;
 import org.objectweb.asm.Opcodes;
 import org.objectweb.asm.tree.AbstractInsnNode;
 import org.objectweb.asm.tree.ClassNode;
+import org.objectweb.asm.tree.FieldInsnNode;
 import org.objectweb.asm.tree.InsnList;
+import org.objectweb.asm.tree.InsnNode;
+import org.objectweb.asm.tree.IntInsnNode;
 import org.objectweb.asm.tree.LdcInsnNode;
 import org.objectweb.asm.tree.MethodInsnNode;
 import org.objectweb.asm.tree.MethodNode;
+import org.objectweb.asm.tree.TypeInsnNode;
 
 public class Obfuscator {
     private static final Map<String, byte[]> f = new HashMap<>();
     private static final List<ClassNode> c = new ArrayList<>();
+
+    public static List<Data> Datas;
 
     // Obfuscar las cadenas de textos true(si lo obfuscara) false(no)
     private static boolean string = true;
@@ -53,6 +60,7 @@ public class Obfuscator {
         run("C:\\Users\\akais\\Desktop\\CrackedObfuscator-FreeVersion\\Cracked-2.2-release.jar");
     }
 
+    public static int counter = 0;
     public static void run(String path) {
         File file = new File(path);
 
@@ -89,12 +97,17 @@ public class Obfuscator {
         }
 
         for (ClassNode classNode : c) {
+            counter = 0;
             if (classNode.name.startsWith(mixin.replace(".", "/"))) continue;
             if (check_Ignore_Class(classNode)) continue;
-            
             if (string) {
                 MethodNode WN;
                 if (!((classNode.access & Opcodes.ACC_INTERFACE) != 0) && !((classNode.access & Opcodes.ACC_ABSTRACT) != 0) && !((classNode.access & Opcodes.ACC_ANNOTATION) != 0) && !((classNode.access & Opcodes.ACC_ENUM) != 0) && !((classNode.access & Opcodes.ACC_RECORD) != 0)) {
+                    String fieldName = "cache";
+                    String nameMethod = "decrypt";
+                    String nms = "decrypt";
+                    classNode.visitField(Opcodes.ACC_PUBLIC | Opcodes.ACC_STATIC | Opcodes.ACC_FINAL, fieldName, "[Ljava/lang/String;", null, null);
+                    Datas = new ArrayList<>(); 
                     String methodName = get(new Random().nextInt(20));
                     for (MethodNode methodNode : classNode.methods) {
                         if (check_Ignore_ClassMethod(classNode, methodNode)) continue;
@@ -107,8 +120,16 @@ public class Obfuscator {
                                 if (ldcInsnNode.cst instanceof String) {
                                     String s = (String)ldcInsnNode.cst;
                                     InsnList il = new InsnList();
-                                    il.add(new LdcInsnNode(Base64.getEncoder().encodeToString(s.getBytes(StandardCharsets.UTF_8))));
-                                    il.add(new MethodInsnNode(Opcodes.INVOKESTATIC, classNode.name, methodName, "(Ljava/lang/String;)Ljava/lang/String;", false));
+                                    
+                                    il.add(new FieldInsnNode(Opcodes.GETSTATIC, classNode.name, fieldName, "[Ljava/lang/String;"));
+                                    il.add(new LdcInsnNode(counter));
+                                    il.add(new InsnNode(Opcodes.AALOAD));
+
+                                    String k = "key";
+
+                                    Datas.add(new Data( encrypt(s, k), k, counter));
+
+                                    counter++;
                                     methodNode.instructions.insert(ldcInsnNode, il);
                                     methodNode.instructions.remove(ldcInsnNode);
                                 }
@@ -116,25 +137,49 @@ public class Obfuscator {
                         }
                     }
 
-                    WN = new MethodNode(Opcodes.ACC_PUBLIC + Opcodes.ACC_STATIC, methodName, "(Ljava/lang/String;)Ljava/lang/String;", null, null);
-                    WN.visitCode();
-                    WN.visitTypeInsn(Opcodes.NEW, "java/lang/String");
-                    WN.visitInsn(Opcodes.DUP);
-                    WN.visitMethodInsn(Opcodes.INVOKESTATIC, "java/util/Base64", "getDecoder", "()Ljava/util/Base64$Decoder;", false);
-                    WN.visitVarInsn(Opcodes.ALOAD, 0);
-                    WN.visitFieldInsn(Opcodes.GETSTATIC, "java/nio/charset/StandardCharsets", "UTF_8", "Ljava/nio/charset/Charset;");
-                    WN.visitMethodInsn(Opcodes.INVOKEVIRTUAL, "java/lang/String", "getBytes", "(Ljava/nio/charset/Charset;)[B", false);
-                    WN.visitMethodInsn(Opcodes.INVOKEVIRTUAL, "java/util/Base64$Decoder", "decode", "([B)[B", false);
-                    WN.visitFieldInsn(Opcodes.GETSTATIC, "java/nio/charset/StandardCharsets", "UTF_8", "Ljava/nio/charset/Charset;");
-                    WN.visitMethodInsn(Opcodes.INVOKESPECIAL, "java/lang/String", "<init>", "([BLjava/nio/charset/Charset;)V", false);
-                    WN.visitInsn(Opcodes.ARETURN);
-                    WN.visitMaxs(5, 2);
-                    WN.visitEnd();
-                    classNode.methods.add(WN);
+                    WN = get(classNode, "<clinit>");
+                    if (WN == null) {
+                        WN = new MethodNode(Opcodes.ACC_STATIC, "<clinit>", "()V", null, null);
+                        classNode.methods.add(WN);
+                    }
+        
+                    InsnList instructions = new InsnList();
+                    instructions.add(new IntInsnNode(Opcodes.BIPUSH, Datas.size()));
+                    instructions.add(new TypeInsnNode(Opcodes.ANEWARRAY, "java/lang/String"));
+                    instructions.add(new FieldInsnNode(Opcodes.PUTSTATIC, classNode.name, fieldName, "[Ljava/lang/String;"));
+            
+                    for (Data sb27Data : Datas) {
+                        instructions.add(new FieldInsnNode(Opcodes.GETSTATIC, classNode.name, fieldName, "[Ljava/lang/String;"));
+                        instructions.add(new InsnNode(Opcodes.DUP));
+                        instructions.add(new LdcInsnNode(sb27Data.getNumber()));
+                        instructions.add(new LdcInsnNode(sb27Data.getData_String()));
+                        instructions.add(new LdcInsnNode(sb27Data.getKey_String()));
+                        instructions.add(new MethodInsnNode(Opcodes.INVOKESTATIC, classNode.name, nameMethod, "(Ljava/lang/String;Ljava/lang/String;)Ljava/lang/String;", false));
+                        instructions.add(new InsnNode(Opcodes.AASTORE));
+                    }
+
+                    for (MethodNode methodNode : classNode.methods) {
+                        if (methodNode.name.equals("<clinit>")) {
+                            if (methodNode.instructions != null && methodNode.instructions.getFirst() != null) {
+                                methodNode.instructions.insert(methodNode.instructions.getFirst(), instructions);
+                            } else {
+                                methodNode.instructions.add(instructions);
+                            }
+                            break;
+                        }
+                    }
+
+                    try {
+                        MethodNode m = get(t(), nms);
+                        m.name = methodName;
+                        classNode.methods.add(m);   
+                    } catch (Exception e) {
+                        // TODO: handle exception
+                    }
                 }
 
                 if (((classNode.access & Opcodes.ACC_RECORD) != 0)) continue;
-                WN = new MethodNode(Opcodes.ACC_PROTECTED, "PROTECTED" + getS(57) + "____HELLO_WORLD___", "()Ljava/lang/String;", null, null);
+                WN = new MethodNode(Opcodes.ACC_PROTECTED, "PROTECTED____HELLO_WORLD___", "()Ljava/lang/String;", null, null);
                 WN.visitCode();
                 WN.visitLdcInsn("https://github.com/XG2025-Akaishin");
                 WN.visitVarInsn(Opcodes.ASTORE, 0);
@@ -170,6 +215,15 @@ public class Obfuscator {
         }
     }
 
+    public static ClassNode t() throws IOException {
+        final ClassReader cr = new ClassReader(Obfuscator.class.getResourceAsStream("/" + Obfuscator.class.getName().replace('.', '/') + ".class"));
+        final ClassNode cn = new ClassNode();
+
+        cr.accept(cn, 0);
+
+        return cn;
+    }
+
     public static String get(int r) {
         StringBuilder sb = new StringBuilder();
         sb.append("_");
@@ -179,14 +233,6 @@ public class Obfuscator {
             } else {
                 sb.append("I");
             }
-        }
-        return sb.toString();
-    }
-
-    public static String getS(int r) {
-        StringBuilder sb = new StringBuilder();
-        for (int j = 0; j < r; j++) {
-            sb.append("\u00a0");
         }
         return sb.toString();
     }
@@ -219,6 +265,60 @@ public class Obfuscator {
             writer = new ClassWriter(0);
             classNode.accept(writer);
             return writer.toByteArray();
+        }
+    }
+
+    public static MethodNode get(final ClassNode classNode, final String strin) {
+        for (final MethodNode method : classNode.methods)
+            if (method.name.equals(strin))
+                return method;
+        return null;
+    }
+
+    public static String decrypt(String obj, String key) {
+        obj = new String(Base64.getDecoder().decode(obj.getBytes(StandardCharsets.UTF_8)), StandardCharsets.UTF_8);
+        StringBuilder sb = new StringBuilder();
+        char[] keyChars = key.toCharArray();
+        int i = 0;
+        for (char c : obj.toCharArray()) {
+            sb.append((char) (c ^ keyChars[i % keyChars.length]));
+            i++;
+        }
+        return sb.toString();
+    }
+
+    public static String encrypt(String obj, String key) {
+        StringBuilder sb = new StringBuilder();
+        char[] keyChars = key.toCharArray();
+        int i = 0;
+        for (char c : obj.toCharArray()) {
+            sb.append((char) (c ^ keyChars[i % keyChars.length]));
+            i++;
+        }
+        return new String(Base64.getEncoder().encode(sb.toString().getBytes(StandardCharsets.UTF_8)), StandardCharsets.UTF_8);
+    }
+
+    public static class Data {
+        private String data_String;
+        private String key_String;
+        private int number;
+    
+        public Data(String data_String, String key_String, int number) {
+            this.data_String = data_String;
+            this.key_String = key_String;
+            this.number = number;
+        }
+    
+        public String getData_String() {
+            return this.data_String;
+        }
+    
+        public String getKey_String() {
+            return this.key_String;
+        }
+    
+        public int getNumber() {
+            return this.number;
         }
     }
 }
